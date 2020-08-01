7e529bdabf3d9983ba35628a6d1d53450700052f
olcaycanitez <olcaycanitez@gmail.com>
2020-08-01 08:32:47 -0700

    add HappyB20.java

---
+++ src/HappyB20.java
@@ -0,0 +1,11 @@
+public class HappyB20 {
+
+    public static void main(String[] args) {
+        System.out.println("B20 is always happy");
+
+
+
+
+    }
+
+}
---
+++ src/HelloB20.java
@@ -0,0 +1,11 @@
+public class HelloB20 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello B20, it is my pleasure to be with you!");
+
+
+        // add a line
+
+    }
+
+}
